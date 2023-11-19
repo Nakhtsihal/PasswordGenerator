013c55b1dc143848eaa6851f35cb9a16ca0866d9
Nakhtsihal <nakhtsihal@yandex.ru>
2023-11-19 23:46:53 +0200

    finish adds

--- Generator.py
+++ Generator.py
@@ -9,11 +9,11 @@ chars = " "
 
 n = int(input("Введите количество паролей для генерации: "))
 length = int(input("Введите длину пароля: "))
-add_digit = input("Включить цифры? (да = да, нет = нет) ").strip()
-add_lowercase = input("Включить прописные буквы?").strip()
-add_uppercase = input("Включить строчные буквы?").strip()
-add_punctuation = input("Включить символы, такие как !#$%&*+-=?@^_?").strip()
-remove_badsymbols = input("Исключить символы il1Lo0O?").strip()
+add_digit = input("Включить цифры? (да | нет ) ").strip()
+add_lowercase = input("Включить прописные буквы?  (да | нет )").strip()
+add_uppercase = input("Включить строчные буквы?  (да | нет )").strip()
+add_punctuation = input("Включить символы, такие как !#$%&*+-=?@^_?  (да | нет )").strip()
+remove_badsymbols = input("Исключить символы il1Lo0O?  (да | нет )").strip()
 
 if add_digit.lower() == "да":
     chars += digits
@@ -22,6 +22,7 @@ if add_lowercase.lower() == "да":
 if add_uppercase.lower() == "да":
     chars += uppercase_letters
 if add_punctuation.lower() == "да":
+    chars += punctuation
 if remove_badsymbols.lower() == "да":
     for c in "il1Lo0O":
         chars = chars.replace(c, "")
@@ -30,7 +31,7 @@ def generate_password(length, chars):
     password = ""
     for j in range(length):
         password += random.choice(chars)
-    return password
+    print(password)
 
 for _ in range(n):
     generate_password(length, chars)
