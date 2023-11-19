b5f62c91342af4aaa4a1b95086505a9e01f510e1
Nakhtsihal <nakhtsihal@yandex.ru>
2023-11-20 00:00:45 +0200

    Changed to English language

--- Generator.py
+++ Generator.py
@@ -7,23 +7,23 @@ punctuation = "!#$%&*+-=?@^_"
 
 chars = " "
 
-n = int(input("Введите количество паролей для генерации: "))
-length = int(input("Введите длину пароля: "))
-add_digit = input("Включить цифры? (да | нет ) ").strip()
-add_lowercase = input("Включить прописные буквы?  (да | нет )").strip()
-add_uppercase = input("Включить строчные буквы?  (да | нет )").strip()
-add_punctuation = input("Включить символы, такие как !#$%&*+-=?@^_?  (да | нет )").strip()
-remove_badsymbols = input("Исключить символы il1Lo0O?  (да | нет )").strip()
+n = int(input("Enter number of passwords: "))
+length = int(input("Enter length of passwords: "))
+add_digit = input("Add digits? (yes | no)>>>").strip()
+add_lowercase = input("Add upper letters?  (yes | no)>>>").strip()
+add_uppercase = input("Add lower letters?  (yes | no)>>>").strip()
+add_punctuation = input("Add symbols like? : !#$%&*+-=?@^_? (yes | no)>>>").strip()
+remove_badsymbols = input("Exclude symbols? : il1Lo0O?  (yes | no)>>>").strip()
 
-if add_digit.lower() == "да":
+if add_digit.lower() == "yes":
     chars += digits
-if add_lowercase.lower() == "да":
+if add_lowercase.lower() == "yes":
     chars += lowercase_letters
-if add_uppercase.lower() == "да":
+if add_uppercase.lower() == "yes":
     chars += uppercase_letters
-if add_punctuation.lower() == "да":
+if add_punctuation.lower() == "yes":
     chars += punctuation
-if remove_badsymbols.lower() == "да":
+if remove_badsymbols.lower() == "yes":
     for c in "il1Lo0O":
         chars = chars.replace(c, "")
 
